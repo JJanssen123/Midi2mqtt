6e3393d7d41c745df710af8767859413ddef984f
JJanssen123 <47112000+JJanssen123@users.noreply.github.com>
2025-01-05 21:10:32 +0100

    added virtual device

--- src/midi2mqtt.py
+++ src/midi2mqtt.py
@@ -24,6 +24,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
     print("Connected to mqtt server with result code " + str(reason_code))
 
     if midi_in_device and midi_in_device != '' and mqtt_publish_topic and mqtt_publish_topic != '':
+        global input_port
         print()
         input_port = mido.open_input(midi_in_device)
         print ("Listening for midi on device", midi_in_device)  
@@ -31,11 +32,12 @@ def on_connect(client, userdata, flags, reason_code, properties):
         print ("Sending midi to mqtt topic", mqtt_publish_topic)
 
     if mqtt_listen_topic and mqtt_listen_topic != '' and midi_out_device and midi_out_device != '':
+        global output_port
         print()
         client.subscribe(mqtt_listen_topic)
         client.on_message = on_message
         print("Listening for midi on mqtt topic", mqtt_listen_topic)  
-        output_port = mido.open_output(midi_out_device)
+        output_port = mido.open_output(midi_out_device, make_virtual_device)
         print("Sending midi to device", midi_out_device)
 
 # Callback for messages on the mqtt-topic
@@ -45,8 +47,7 @@ def on_message(client, userdata, msg):
     event = args['event']
     del args['event']
     message = mido.Message(event, **args)
-    if midi_out_device and midi_out_device != '':  
-        output_port.send(message)
+    output_port.send(message)
 
 
 # Callback for messages on the MIDI-IN device
@@ -63,9 +64,13 @@ def publish_to_mqtt_topic(message):
         client.publish(mqtt_publish_topic, payload=json_str)  
 
 # make mqtt connection
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-if mqtt_username and mqtt_password and mqtt_username != '' and mqtt_password != '':
-    client.username_pw_set(mqtt_username,mqtt_password)
-client.on_connect = on_connect
-client.connect(mqtt_server, mqtt_port, 60)
-client.loop_forever()
+if (midi_in_device and midi_in_device != '' and mqtt_publish_topic and mqtt_publish_topic != '') or (mqtt_listen_topic and mqtt_listen_topic != '' and midi_out_device and midi_out_device != ''):
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    if mqtt_username and mqtt_password and mqtt_username != '' and mqtt_password != '':
+        client.username_pw_set(mqtt_username,mqtt_password)
+    client.on_connect = on_connect
+    client.connect(mqtt_server, mqtt_port, 60)
+    client.loop_forever()
+else:
+    print()
+    print ("No devices and mqtt-topics set in settings.py. Copy the name of the device from above.")
--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,16 @@
 # ======================== SETTINGS =============================
 
-# settings for sending from device to mqtt, leave empty if you only want to listen to mqtt:
+# Settings for sending from device to mqtt, leave empty if you only want to listen to mqtt:
 midi_in_device = ''  
-mqtt_publish_topic = ''
+mqtt_publish_topic = 'midi/j'
 
-# settings for listening on mqtt, leave empty if you only want to send from device to mqtt:
-midi_out_device = 'Microsoft GS Wavetable Synth 0'
+# Settings for listening on mqtt, leave empty if you only want to send from device to mqtt:
+midi_out_device = 'Microsoft GS Wavetable Synth 0'  # on MacOS and Linux this can be a virtual device , see below
 mqtt_listen_topic = 'midi/#'
+
+# If you want to send the midi to a DAW instead of a device, you can make a virtual MIDI-device.
+# However this only works on MacOS and Linux. On Windows, use a tool like loopMIDI from https://www.tobias-erichsen.de/software/loopmidi.html.
+make_virtual_device = False
 
 # mqtt server settings
 mqtt_server = 'test.mosquitto.org'
